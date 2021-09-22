18993b7b6a6181a4b36bd2b572d44ff3b9141da2
Paul Ologeh <ologehmamezi@gmail.com>
2021-09-22 03:12:34 +0100

    fixed label editing for touch devices

--- front-end/src/components/EditableLabel.jsx
+++ front-end/src/components/EditableLabel.jsx
@@ -45,6 +45,9 @@ export const EditableLabel = ({
         onClick={(e) => {
           setView("text");
         }}
+        onTouchEnd={(e) => {
+          setView("text");
+        }}
       >
         {value}
       </span>
