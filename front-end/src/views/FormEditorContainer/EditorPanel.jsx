7dcb3749971d269ac4780d9ed3468f122b952561
Paul Ologeh <ologehmamezi@gmail.com>
2021-09-22 18:17:05 +0100

    completed basi form editor

--- front-end/src/views/FormEditorContainer/EditorPanel.jsx
+++ front-end/src/views/FormEditorContainer/EditorPanel.jsx
@@ -6,7 +6,7 @@ import cloneDeep from "lodash/cloneDeep";
 export const EditorPanel = ({ mobile, height = 200 }) => {
   const [toolStore, updateToolStore] = useContext(ToolStore);
 
-  const onToolDelete = (event) => {
+  const onTrash = (event) => {
     let oldToolStore = cloneDeep(toolStore);
     let toolId = oldToolStore.selectedTool;
     oldToolStore.allToolProps = oldToolStore.allToolProps.filter(
@@ -19,36 +19,45 @@ export const EditorPanel = ({ mobile, height = 200 }) => {
     console.debug(toolId, `deleted ${toolId} from store ${oldToolStore}`);
   };
 
+  const onRefresh = (event) => {
+    let oldToolStore = cloneDeep(toolStore);
+    oldToolStore.selectedTool = "";
+    oldToolStore.allToolProps = [];
+    oldToolStore.allTools = [];
+    updateToolStore(oldToolStore);
+    console.debug(`cleared the store`);
+  };
+
   const renderMobile = () => {
     return (
       <div style={{ alignSelf: "center", marginLeft: 10 }}>
-        <Button circular icon="trash" onClick={onToolDelete}></Button>
-        <Button circular icon="refresh"></Button>
-        <Button circular icon="redo"></Button>
-        <Button circular icon="undo"></Button>
-        <Button circular icon="bold"></Button>
-        <Button circular icon="italic"></Button>
-        <Button circular icon="underline"></Button>
-        <Button circular icon="font"></Button>
-        <Button circular icon="copy"></Button>
-        <Button circular icon="paste"></Button>
+        <Button basic circular icon="trash" onClick={onTrash}></Button>
+        <Button basic circular icon="refresh" onClick={onRefresh}></Button>
+        <Button disabled basic circular icon="redo"></Button>
+        <Button disabled basic circular icon="undo"></Button>
+        <Button disabled basic circular icon="bold"></Button>
+        <Button disabled basic circular icon="italic"></Button>
+        <Button disabled basic circular icon="underline"></Button>
+        <Button disabled basic circular icon="font"></Button>
+        <Button disabled basic circular icon="copy"></Button>
+        <Button disabled basic circular icon="paste"></Button>
       </div>
     );
   };
 
   const renderDesktop = () => {
     return (
-      <Button.Group>
-        <Button circular icon="trash" onClick={onToolDelete}></Button>
-        <Button circular icon="refresh"></Button>
-        <Button circular icon="redo"></Button>
-        <Button circular icon="undo"></Button>
-        <Button circular icon="bold"></Button>
-        <Button circular icon="italic"></Button>
-        <Button circular icon="underline"></Button>
-        <Button circular icon="font"></Button>
-        <Button circular icon="copy"></Button>
-        <Button circular icon="paste"></Button>
+      <Button.Group basic>
+        <Button circular icon="trash" onClick={onTrash}></Button>
+        <Button circular icon="refresh" onClick={onRefresh}></Button>
+        <Button disabled circular icon="redo"></Button>
+        <Button disabled circular icon="undo"></Button>
+        <Button disabled circular icon="bold"></Button>
+        <Button disabled circular icon="italic"></Button>
+        <Button disabled circular icon="underline"></Button>
+        <Button disabled circular icon="font"></Button>
+        <Button disabled circular icon="copy"></Button>
+        <Button disabled circular icon="paste"></Button>
       </Button.Group>
     );
   };
