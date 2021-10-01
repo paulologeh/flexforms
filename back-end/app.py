4f9aa5b65969135334ac5dfa597aedadaedf0a09
Paul Ologeh <ologehmamezi@gmail.com>
2021-10-01 21:38:02 +0100

    making app heroku compatiable

--- back-end/app.py
+++ back-end/app.py
@@ -11,38 +11,30 @@ from api.forms import SavedForms, FilledForms
 load_dotenv()
 
 
-def create_app():
-    app = Flask(__name__)
-    app.debug = config.get('APP', 'DEBUG')
-    app.config['CORS_HEADERS'] = 'Content-Type'
-    api = Api(app)
+today = datetime.datetime.now().strftime("%Y%m%d")
+log_path = f"logs/{today}"
+if not os.path.isdir(log_path):
+    os.mkdir(log_path)
 
-    CORS(app, resources={'/*': {'origins': ['.*']} })
+pid = os.getpid()
+log_file = f"{log_path}/app.log"
+config = configparser.ConfigParser()
+config.read('config.ini')
+logging.basicConfig(filename=log_file, level=int(config.get('LOGS', 'LEVEL')),
+                    format='%(asctime)s - [%(levelname)s] %(name)s [%(module)s.%(funcName)s:%(lineno)d]: %('
+                        'message)s',
+                    datefmt='%Y%m%d-%H:%M:%S')
+logger = logging.getLogger(__name__)
 
-    # API Resource routing
-    api.add_resource(FilledForms, '/api/v1/filledforms')
-    api.add_resource(SavedForms, '/api/v1/savedforms')
-
-    return app
+app = Flask(__name__)
+app.debug = config.get('APP', 'DEBUG')
+app.config['CORS_HEADERS'] = 'Content-Type'
+api = Api(app)
+CORS(app, resources={'/*': {'origins': ['.*']} })
+# API Resource routing
+api.add_resource(FilledForms, '/api/v1/filledforms')
+api.add_resource(SavedForms, '/api/v1/savedforms')
 
 
 if __name__ == '__main__':
-    today = datetime.datetime.now().strftime("%Y%m%d")
-    log_path = f"logs/{today}"
-
-    if not os.path.isdir(log_path):
-        os.mkdir(log_path)
-
-    pid = os.getpid()
-    log_file = f"{log_path}/app.log"
-
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    logging.basicConfig(filename=log_file, level=int(config.get('LOGS', 'LEVEL')),
-                        format='%(asctime)s - [%(levelname)s] %(name)s [%(module)s.%(funcName)s:%(lineno)d]: %('
-                            'message)s',
-                        datefmt='%Y%m%d-%H:%M:%S')
-    logger = logging.getLogger(__name__)
-    app = create_app()
-    
     app.run(port=config.get('APP', 'PORT'), threaded=True)
