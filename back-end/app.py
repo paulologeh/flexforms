6f5431d80ddfd55eff694e4e0d5a4ed56f4e3947
Paul Ologeh <paulologeh@protonmail.com>
2023-11-27 18:06:35 +0000

    dockerise application

--- back-end/app.py
+++ back-end/app.py
@@ -11,19 +11,9 @@ from api.forms import SavedForms, FilledForms
 load_dotenv()
 
 
-today = datetime.datetime.now().strftime("%Y%m%d")
-log_path = f"logs/{today}"
-if not os.path.isdir(log_path):
-    os.mkdir(log_path)
-
-pid = os.getpid()
-log_file = f"{log_path}/app.log"
 config = configparser.ConfigParser()
 config.read('config.ini')
-logging.basicConfig(filename=log_file, level=int(config.get('LOGS', 'LEVEL')),
-                    format='%(asctime)s - [%(levelname)s] %(name)s [%(module)s.%(funcName)s:%(lineno)d]: %('
-                        'message)s',
-                    datefmt='%Y%m%d-%H:%M:%S')
+
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
